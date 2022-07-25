df3e581c13072ee6bf0a87687d7aeee839fe5606
GitHub Action <action@github.com>
2022-07-25 22:06:23 +0000

    Template cleanup

--- src/main/kotlin/com/github/vanessacramoran/plugindev/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/vanessacramoran/plugindev/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.vanessacramoran.plugindev.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.vanessacramoran.plugindev.MyBundle
 
 class MyApplicationService {
 
--- src/test/kotlin/com/github/vanessacramoran/plugindev/MyPluginTest.kt
+++ src/test/kotlin/com/github/vanessacramoran/plugindev/MyPluginTest.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.vanessacramoran.plugindev
 
 import com.intellij.ide.highlighter.XmlFileType
 import com.intellij.psi.xml.XmlFile
